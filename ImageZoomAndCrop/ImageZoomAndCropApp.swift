2bceb1cabd32629b4461b19a38b3f86811338932
Daniel Taylor English <danieltaylorenglish@gmail.com>
2022-11-03 15:07:07 -0700

    Initial Commit

---
+++ ImageZoomAndCrop/ImageZoomAndCropApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageZoomAndCropApp.swift
+//  ImageZoomAndCrop
+//
+//  Created by Daniel Taylor English on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ImageZoomAndCropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
